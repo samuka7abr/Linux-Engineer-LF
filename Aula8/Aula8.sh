fb5948e0cc9db1f81ce1f4a73f074bf0d8e44615
samuka7abr <samuelabrao2006@gmail.com>
2025-07-06 16:24:10 -0300

    Aula: 8 - Arrumando confusão do Vim

--- Aula8/Aula8.sh
+++ Aula8/Aula8.sh
@@ -1,3 +1,12 @@
 #!bin/bash
 
 
+lshw #traz todas as informações do seu hardware
+hwinfo #faz a mesma coisa, mas o display de informações é diferente
+
+fdisk /dev/disco_desejado
+#abre o menu para fazer a partição do disco
+#(para mais detalhes, apertar m)
+
+mount <partição> <local> #faz o vinculo do dispositivo ao diretorio
+
